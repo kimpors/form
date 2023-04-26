a75f21bfe121efd13590ac122b63d6a5b019238a
kimpors <mikita_korshak@gmail.com>
2023-04-26 14:12:48 +0300

    Додав більше прикладів

--- script.js
+++ script.js
@@ -1,27 +1,54 @@
-const form = document.getElementById("solve");
+const form = document.getElementsByTagName("form")[0];
 const result = document.getElementById("result");
-const a = form.elements["a"];
-const b = form.elements["b"];
-const c = form.elements["c"];
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const D = Math.pow(b.value, 2) - 4 * a.value * c.value;
+function solve() {
+  const a = form.elements["a"].value;
+  const b = form.elements["b"].value;
+  const c = form.elements["c"].value;
+
+  const D = Math.pow(b, 2) - 4 * a * c;
   let solve;
 
   if (D > 0) {
-    const x1 = ((-b.value - Math.sqrt(D)) / (2 * a.value)).toPrecision(2);
-    const x2 = ((-b.value + Math.sqrt(D)) / (2 * a.value)).toPrecision(2);
+    const x1 = ((-b - Math.sqrt(D)) / (2 * a)).toPrecision(2);
+    const x2 = ((-b + Math.sqrt(D)) / (2 * a)).toPrecision(2);
 
     solve = "X1: " + x1 + ", X2: " + x2;
   } else if (D == 0) {
-    const x = (-b.value / (2 * a.value)).toPrecision(3);
+    const x = (-b / (2 * a)).toPrecision(3);
     solve = "X: " + x;
   } else {
     solve = "Коренів немає";
   }
 
   result.innerHTML = "<h2>Результат</h2> <p>" + solve + "</p>";
+}
+
+function login() {
+  const nickname = form.elements["nickname"].value;
+  const password = form.elements["password"].value;
+  if (nickname == "admin" && password == "admin") {
+    result.innerHTML = "<h2>Вірний логін</h2>";
+  } else {
+    result.innerHTML = "<h2>Невірний логін</h2>";
+  }
+}
+
+function send() {
+  result.innerHTML = "<h2>Результат</h2>";
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (form.id == "math") {
+    solve();
+  } else if (form.id == "login") {
+    login();
+  } else if (form.id == "test") {
+    send();
+  }
+
   window.scrollTo(result.offsetLeft, result.offsetTop);
   return false;
 });
