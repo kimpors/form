d0f30208a3db9348eb26f6a8a79c400e5bad8929
kimpors <mikita_korshak@ukr.net>
2024-01-12 15:29:57 +0200

    Implement UI elements, and now it's fully moved

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './style.css'
 import Nav from './components/Nav/Nav'
+import './style.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Nav />
-        {children}
+        { children }
       </body>
     </html>
   )
