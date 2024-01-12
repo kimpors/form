1cb40196d00d83f1373bd0bf1ce4e897e570af95
kimpors <mikita_korshak@ukr.net>
2024-01-12 12:59:17 +0200

    Implement login section

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,40 @@
+'use client'
+
+import { FormEvent, useState } from 'react'
+
 const Login = () =>
 {
+  const [name, setName] = useState('')
+  const [pass, setPass] = useState('')
+  const [result, setResult] = useState('')
+
+  function login(e: FormEvent) {
+    e.preventDefault();
+
+    if (name == "admin" && pass == "admin") {
+      setResult("Success");
+    } else {
+      setResult("Wrong login or password")
+    }
+  }
+
   return(
-    <h1>Login</h1>
+    <article>
+      <form onSubmit={ login }>
+        <h2>Login</h2>
+
+        <p>
+          <label>Nickname</label>
+          <input placeholder="admin" required value={ name } onChange={ (e) => setName(e.target.value) }/>
+        </p>
+        <p>
+          <label>Password</label>
+          <input type="password" placeholder="admin" required value={ pass } onChange={ (e) => setPass(e.target.value) }/>
+        </p>
+        <button type="submit">Enter</button>
+      </form>
+      <section><h2>{ result }</h2></section>
+    </article>
   )
 }
 
